c9e32073bcd37eaa3c249fb4dbd7f9f3a28c710f
simmhugs <simon.graetz@gmx.de>
2025-01-21 15:35:54 +0100

    working to display an scroll the answer of the LLM

--- aica_streamy/state/state.py
+++ aica_streamy/state/state.py
@@ -7,6 +7,20 @@ import reflex.event as ev
 
 import aica_streamy.content as content
 
+import os
+from dotenv import dotenv_values, load_dotenv
+
+load_dotenv()
+if not os.getenv("GEMINI_API_KEY"):
+    raise Exception("Please set GEMINI_API_KEY environment variable.")
+
+keys = {key: value for key, value in dotenv_values().items()}
+import google.generativeai as genai
+
+genai.configure(api_key=keys["GEMINI_API_KEY"])
+model = genai.GenerativeModel("gemini-2.0-flash-exp")
+chat = model.start_chat(history=[])
+
 BOTTOM_ELEMENT_ID = "bottom"
 
 
@@ -23,9 +37,12 @@ class ScrollHandlingState(rx.State):
             f"document.getElementById('{BOTTOM_ELEMENT_ID}').scrollIntoView()"
         )
 
+
 class MessageGenerator(rx.State):
-    parts = list(content.markdown2)
-    index = 0
+    # parts = list(content.markdown2)
+    parts: list[str] = []
+    done: bool = False
+    index: int = 0
     question: list[str]
     messages: list[Message]
     should_load: bool = False
@@ -34,7 +51,7 @@ class MessageGenerator(rx.State):
     @rx.event
     def process_question(self, data: dict[str, Any]):
         self.question = list(data["question"])
-        print(self.question)
+        # print(self.question)
         return MessageGenerator.start
 
     @rx.event
@@ -60,39 +77,64 @@ class MessageGenerator(rx.State):
     @rx.event(background=True)
     async def add_question(self):
         while self.should_load:
-            async with self:                
+            async with self:
                 if self.messages == []:
                     self.messages.append(Message(sub_type="question", text=""))
 
                 if self.index < len(self.question):
                     self.messages[-1].text += f"{self.question[self.index]}"
-                    print(f"{self.index=}") 
-                    print(f"'{self.messages[-1].text}'")
                     self.index += 1
                 else:
-                    print(f"{self.index=}")                    
                     self.index = 0
                     self.question = []
                     break
-                
-            await asyncio.sleep(0.06)
+
+            await asyncio.sleep(0.03)
             yield ScrollHandlingState.scroll_to_bottom
         yield MessageGenerator.add_answer
-            
+
     @rx.event(background=True)
-    async def add_answer(self):
+    async def add_answer2(self):
         while self.should_load:
-            async with self:                
+            async with self:
                 if self.messages[-1].sub_type == "question":
                     self.messages.append(Message(sub_type="answer", text=""))
-                
+
                 if self.index < len(self.parts):
                     self.messages[-1].text += f"{self.parts[self.index]}"
                     self.index += 1
                 else:
                     self.index = 0
-                    self.should_load = False                    
+                    self.should_load = False
                     break
+
             await asyncio.sleep(0.06)
             yield ScrollHandlingState.scroll_to_bottom
-            
+
+    @rx.event(background=True)
+    async def add_answer(self):
+        response = chat.send_message(
+            self.messages[-1].text,
+            stream=True,
+        )
+        async with self:
+            for chunk in response:
+                if (answer := chunk.text) is not None:
+                    for p in list(answer):
+                        self.parts.append(p)
+            self.done = True
+        while self.should_load:
+            async with self:
+                if self.messages[-1].sub_type == "question":
+                    self.messages.append(Message(sub_type="answer", text=""))
+
+                if self.index < len(self.parts):
+                    self.messages[-1].text += f"{self.parts[self.index]}"
+                    self.index += 1
+                elif self.done:
+                    self.index = 0
+                    self.should_load = False
+                    break
+
+            await asyncio.sleep(0.015)
+            yield ScrollHandlingState.scroll_to_bottom
