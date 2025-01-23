ab6885a626b87b73af6659f0fe636bbb679da671
simmhugs <simon.graetz@gmx.de>
2025-01-23 21:20:43 +0100

    refactor to make file more clear

--- aica_streamy/aica_streamy.py
+++ aica_streamy/aica_streamy.py
@@ -22,66 +22,19 @@ def copy_button(message: Message) -> rx.Component:
                 rx.hstack(
                     rx.el.button(
                         rx.icon(tag="copy", size=18),
-                        on_click=[
-                            rx.set_clipboard(message.text),
-                            rx.toast("Copied!"),
-                        ],
-                        _hover={"color": rx.color("accent", 8)},
                     ),
-                    rx.text("copy"),
+                    rx.text(
+                        "copy",
+                    ),
+                    _hover={"color": rx.color("accent", 8)},
+                    on_click=[
+                        rx.set_clipboard(message.text),
+                        rx.toast("Copied answer to clipboard"),
+                    ],
                 ),
             ),
-            margin="10px",
-        ),
-    )
-
-
-def p(message: Message) -> rx.Component:
-    return rx.cond(
-        #rx.box(
-            #rx.cond(
-                True,
-                rx.text("", hidden=True,),
-            ),
-            #hidden=True,
-        #),
-    #)
-def p2(message: Message) -> rx.Component:
-    return rx.text(message)
-
-
-def create_bubble(message: Message) -> rx.Component:
-    return rx.cond(
-        message.sub_type == "answer",
-        rx.box(
-            rx.cond(
-                message.length > 400,
-                copy_button(message),
-                p(message),
-            ),
-            rx.markdown(
-                message.text,
-                margin="20px",
-            ),
-            copy_button(message),
-            width="40vw",
-            border_radius="10px",
-            bg=rx.color("accent", 4),
-            display="inline-block",
-            text_align="left",
-            margin_right="auto",
-        ),
-        rx.box(
-            rx.markdown(
-                message.text,
-            ),
-            width="40vw",
-            padding="20px",
-            border_radius="10px",
-            bg=rx.color("accent", 6),
-            display="inline-block",
-            text_align="left",
-            margin_left="auto",
+            margin_left="5px",
+            margin_bottom="2px",
         ),
     )
 
@@ -92,6 +45,108 @@ def generate_list(message: Message) -> rx.Component:
         message.sub_type == "question",
         rx.box(rx.text(f"question: {m}"), bg=rx.color("accent", 4)),
         rx.box(rx.text(f"answer: {m}"), bg=rx.color("accent", 6)),
+    )
+
+
+def create_bubble(message: Message) -> rx.Component:
+    config = dict(
+        width="40vw", padding="10px", border_radius="20px", display="inline-block"
+    )
+    return rx.cond(
+        message.sub_type == "answer",
+        rx.box(
+            rx.markdown(
+                message.text,
+                margin="20px",
+            ),
+            copy_button(message),
+            bg=rx.color("accent", 4),
+            margin_right="auto",
+            **config,
+        ),
+        rx.box(
+            rx.markdown(
+                message.text,
+            ),
+            bg=rx.color("accent", 6),
+            margin_left="auto",
+            **config,
+        ),
+    )
+
+
+def input() -> rx.Component:
+    return (
+        rx.center(
+            rc.form(
+                rc.form_control(
+                    rx.hstack(
+                        rx.input(
+                            rx.input.slot(
+                                rx.tooltip(
+                                    rx.icon("info", size=18),
+                                    content="Enter a question to get a response.",
+                                )
+                            ),
+                            placeholder="Type something...",
+                            id="question",
+                            width=[
+                                "15em",
+                                "20em",
+                                "45em",
+                                "50em",
+                                "50em",
+                                "50em",
+                            ],
+                        ),
+                        rx.button(
+                            "Send",
+                            loading=MessageGenerator.should_load,
+                            type="submit",
+                            background_color=rx.color("accent", 4),
+                            color=rx.color("accent", 12),
+                        ),
+                        align_items="center",
+                    ),
+                    is_disabled=MessageGenerator.should_load,
+                    padding_left="10px",
+                    padding_right="10px",
+                ),
+                on_submit=MessageGenerator.process_question,
+                reset_on_submit=True,
+                position="sticky",
+                bottom="0",
+                padding_y="16px",
+                backdrop_filter="auto",
+                backdrop_blur="lg",
+                border_top=f"1px solid {rx.color('mauve', 3)}",
+                background_color=rx.color("mauve", 2),
+                align_items="stretch",
+            ),
+        ),
+    )
+
+
+def scroll():
+    return rx.center(
+        rx.scroll_area(
+            rx.vstack(
+                rx.foreach(MessageGenerator.messages, create_bubble),
+                rx.text(
+                    "",
+                    id=BOTTOM_ELEMENT_ID,
+                    visibility="hidden",
+                    margin_top="20xp",
+                ),
+            ),
+            flex="1",
+            id="scroller",
+            width="50vw",
+            height="80vh",
+            padding="10px",
+            margin="10px",
+            type="auto",
+        ),
     )
 
 
@@ -111,76 +166,10 @@ def index() -> rx.Component:
         ),
         rx.vstack(
             rx.tooltip(
-                rx.heading("Stream chat emulator"),
-                content="emulate the response of an LLM from a long markdown string.",
+                rx.heading("LLM Chat"),
             ),
-            rx.center(
-                rx.scroll_area(
-                    rx.vstack(
-                        rx.foreach(MessageGenerator.messages, create_bubble),
-                        rx.text(
-                            "",
-                            id=BOTTOM_ELEMENT_ID,
-                            visibility="hidden",
-                            margin_top="20xp",
-                        ),
-                    ),
-                    flex="1",
-                    id="scroller",
-                    width="50vw",
-                    height="80vh",
-                    padding="10px",
-                    margin="10px",
-                    type="auto",
-                ),
-            ),
-            rx.center(
-                rc.form(
-                    rc.form_control(
-                        rx.hstack(
-                            rx.input(
-                                rx.input.slot(
-                                    rx.tooltip(
-                                        rx.icon("info", size=18),
-                                        content="Enter a question to get a response.",
-                                    )
-                                ),
-                                placeholder="Type something...",
-                                id="question",
-                                width=[
-                                    "15em",
-                                    "20em",
-                                    "45em",
-                                    "50em",
-                                    "50em",
-                                    "50em",
-                                ],
-                            ),
-                            rx.button(
-                                "Send",
-                                loading=MessageGenerator.should_load,
-                                type="submit",
-                                background_color=rx.color("accent", 4),
-                                color=rx.color("accent", 12),
-                            ),
-                            align_items="center",
-                        ),
-                        is_disabled=MessageGenerator.should_load,
-                        padding_left="10px",
-                        padding_right="10px",
-                    ),
-                    on_submit=MessageGenerator.process_question,
-                    reset_on_submit=True,
-                    position="sticky",
-                    bottom="0",
-                    padding_y="16px",
-                    backdrop_filter="auto",
-                    backdrop_blur="lg",
-                    border_top=f"1px solid {rx.color('mauve', 3)}",
-                    background_color=rx.color("mauve", 2),
-                    align_items="stretch",
-                ),
-            ),
+            scroll(),
+            input(),
             align="center",
             min_height="100vh",
         ),
--- aica_streamy/state/state.py
+++ aica_streamy/state/state.py
@@ -5,10 +5,6 @@ from typing import Any
 
 import reflex as rx
 
-# import reflex.event as ev
-
-# import aica_streamy.content as content
-
 import os
 from dotenv import dotenv_values, load_dotenv
 
@@ -17,17 +13,12 @@ if not os.getenv("GEMINI_API_KEY"):
     raise Exception("Please set GEMINI_API_KEY environment variable.")
 
 keys = {key: value for key, value in dotenv_values().items()}
-# import google.generativeai as genai
+
 
 from google import genai
 from google.genai import types
 
-# Only run this block for Gemini Developer API
 client = genai.Client(api_key=keys["GEMINI_API_KEY"])
-# genai.configure(api_key=keys["GEMINI_API_KEY"])
-# model = genai.GenerativeModel("gemini-2.0-flash-exp")
-# chat = model.start_chat(history=[])
-
 
 BOTTOM_ELEMENT_ID = "bottom"
 
@@ -47,51 +38,17 @@ class ScrollHandlingState(rx.State):
             f"document.getElementById('{BOTTOM_ELEMENT_ID}').scrollIntoView()"
         )
 
-
-async def fill_bucket(model, prompt, queue):
-    response = model.generate_content(prompt, stream=True)
-    for chunk in response:
-        print("fill bucket")
-        if (answer := chunk.text) is not None:
-            await queue.put(answer)
-    await queue.put(None)
-
-
-async def empty_bucket(mg, queue):
-    while mg.should_load:
-        print("empty bucket")
-        chunk = await queue.get()
-        if chunk is None:
-            break
-
-        # yield sg.scroll_to_bottom
-        # yield ScrollHandlingState.scroll_to_bottom
-        async with mg:
-            if mg.messages[-1].sub_type == "question":
-                print(f"{time.time()}: added question")
-                mg.messages.append(
-                    Message(sub_type="answer", text="", id=len(mg.messages), length=0)
-                )
-            mg.messages[-1].text += chunk
-
-        # yield ScrollHandlingState.scroll_to_bottom
-        await asyncio.sleep(0.3)
-    # yield ScrollHandlingState.scroll_to_bottom
-
-
 class MessageGenerator(rx.State):
     parts: list[str] = []
     done: bool = False
     index: int = 0
-    question: list[str]
     messages: list[Message]
     should_load: bool = False
-    q: str = ""
+    question: str = ""
 
     @rx.event
     def process_question(self, data: dict[str, Any]):
-        self.q = data["question"]
-        self.question = list(data["question"])
+        self.question = data["question"]
         return MessageGenerator.start
 
     @rx.event
@@ -107,7 +64,8 @@ class MessageGenerator(rx.State):
         return MessageGenerator.add_question
 
     @rx.event(background=True)
-    async def add_question(self):
+    async def add_question2(self):
+        question_characters = list(self.question)
         while self.should_load:
             async with self:
                 if self.messages == [] or self.messages[-1].sub_type == "answer":
@@ -120,106 +78,23 @@ class MessageGenerator(rx.State):
                         )
                     )
 
-                if self.index < len(self.question):
-                    self.messages[-1].text += f"{self.question[self.index]}"
+                if self.index < len(question_characters):
+                    self.messages[-1].text += f"{question_characters[self.index]}"
                     self.messages[-1].length = self.index
                     self.index += 1
                 else:
                     self.index = 0
-                    self.question = []
                     break
 
             yield ScrollHandlingState.scroll_to_bottom
             await asyncio.sleep(0.03)
         yield MessageGenerator.add_answer
-        # yield MessageGenerator.StartProcess
-
-    # @rx.event(background=True)
-    # async def add_answer_old(self):
-    #     response = chat.send_message(
-    #         self.messages[-1].text,
-    #         stream=True,
-    #     )
-    #     async with self:
-    #         self.parts = []
-    #         self.index = 0
-    #         for chunk in response:
-    #             if (answer := chunk.text) is not None:
-    #                 self.parts.append(answer)
-
-    #     while self.should_load:
-    #         yield ScrollHandlingState.scroll_to_bottom
-    #         await asyncio.sleep(0.3)
-    #         async with self:
-    #             if self.messages[-1].sub_type == "question":
-    #                 print(f"{time.time()}: added question")
-    #                 self.messages.append(
-    #                     Message(
-    #                         sub_type="answer", text="", id=len(self.messages), length=0
-    #                     )
-    #                 )
-
-    #             if self.index < len(self.parts):
-    #                 self.messages[-1].text += f"{self.parts[self.index]}"
-    #                 self.messages[-1].length = self.index
-    #                 self.index += 1
-    #             else:
-    #                 self.index = 0
-    #                 self.should_load = False
-    #                 break
-
-    #         yield ScrollHandlingState.scroll_to_bottom
-    #         await asyncio.sleep(0.3)
-    #     yield ScrollHandlingState.scroll_to_bottom
-
-    # @rx.event(background=True)
-    # async def StartProcess(self):
-    #     queue = asyncio.Queue()
-    #     generate_task = asyncio.create_task(
-    #         fill_bucket(model=model, prompt=self.messages[-1].text, queue=queue)
-    #     )
-    #     update_ui_task = asyncio.create_task(empty_bucket(mg=self, queue=queue))
-    #     await asyncio.gather(generate_task, update_ui_task)
-
-    # @rx.event(background=True)
-    # async def add_answer3(self):
-    #     queue = asyncio.Queue()
-
-    #     async def producer():
-    #         response = chat.send_message(self.messages[-1].text, stream=True)
-    #         for chunk in response:
-    #             if (answer := chunk.text) is not None:
-    #                 await queue.put(answer)
-    #         await queue.put(None)  # Signal end of stream
-
-    #     async def consumer():
-    #         async with self:
-    #             self.messages.append(
-    #                 Message(sub_type="answer", text="", id=len(self.messages), length=0)
-    #             )
-
-    #         while True:
-    #             chunk = await queue.get()
-    #             if chunk is None:
-    #                 break
-
-    #             async with self:
-    #                 self.messages[-1].text += chunk
-    #                 self.messages[-1].length += len(chunk)
-
-    #             rx.call_script(
-    #                 f"document.getElementById('{BOTTOM_ELEMENT_ID}').scrollIntoView()"
-    #             )
-
-    #             await asyncio.sleep(0.1)
-
-    #         # self.should_load = False
-    #         rx.call_script(
-    #             f"document.getElementById('{BOTTOM_ELEMENT_ID}').scrollIntoView()"
-    #         )
-
-    #     # Run producer and consumer concurrently
-    #     await asyncio.gather(producer(), consumer())
+    
+    @rx.event
+    def add_question(self):
+        self.messages.append(Message(sub_type="question",text=self.question, id=len(self.messages), length=0))
+        yield ScrollHandlingState.scroll_to_bottom        
+        return MessageGenerator.add_answer
 
     @rx.event(background=True)
     async def add_answer(self):
@@ -230,7 +105,7 @@ class MessageGenerator(rx.State):
 
         async for response in client.aio.models.generate_content_stream(
             model="gemini-2.0-flash-exp",
-            contents=self.q,
+            contents=self.question,
         ):
             for word in str(response.text).split(" "):
                 async with self:
